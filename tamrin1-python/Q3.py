f138ca57ec789077457eb3080c4727f1a8aa3456
sajedehgharabadiyan78 <76538787+sajedehgharabadiyan78@users.noreply.github.com>
2021-11-06 13:16:39 +0330

    Update Q3.py

--- tamrin1-python/Q3.py
+++ tamrin1-python/Q3.py
@@ -5,7 +5,7 @@ BMI=weight/(Height**2)
 
 if BMI < 18.5 :
     print("UNDERWEIGHT!")
-elif BMI > 18.5 and BMI < 24.9 :
+elif BMI >= 18.5 and BMI < 24.9 :
     print("NORMAL!")
 elif BMI > 25 and BMI < 29.9:
     print("OVERWEIGHT!")
