5118b3e65f5607cb460b33c2ed50948f95d33531
SajedehGharabadian <76538787+SajedehGharabadian@users.noreply.github.com>
2022-01-16 13:44:25 +0330

    Delete tamrin10-python directory

--- tamrin10-python/clip.py
+++
@@ -1,6 +0,0 @@
-
-from media_class import Media
-
-class Clip(Media):
-   def __init__(self, t, n, dir, Im, u, dur,act):
-       Media.__init__(self,t, n, dir, Im, u, dur,act)
--- tamrin10-python/series.py
+++
@@ -1,9 +0,0 @@
-
-
-from media_class import Media
-
-class Series(Media):
-    def __init__(self,t,n,dir,Im,u,dur,p,act,s=0):
-        Media.__init__(self,t,n,dir,Im,u,dur,act)
-        self.part = p
-        self.season = s
