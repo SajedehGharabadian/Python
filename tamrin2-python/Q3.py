08292dd9036c591b658c157a9f2e14f28be3ab1f
SajedehGharabadian <76538787+SajedehGharabadian@users.noreply.github.com>
2022-01-16 13:38:39 +0330

    Delete tamrin2-python directory

--- tamrin2-python/Q3.py
+++
@@ -1,9 +0,0 @@
-
-time = int(input("enter time : "))
-
-hour = time // 3600
-reminder_hour = time % 3600
-minutes = reminder_hour // 60
-seconds =reminder_hour  % 60
-
-print(hour , ":" , minutes , ":" , seconds)
